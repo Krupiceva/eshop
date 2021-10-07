84e0a5c0b0d6075cf2b76987e441ed60a4aed6c0
krupiceva <dora.mlinaric94@gmail.com>
2021-10-07 17:22:04 +0200

    Implement fetching products from backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,26 @@
 const express = require("express");
 const app = express();
 
+const products = require("./data/products");
+
+
+app.route("/")
+.get(function(req, res){
+    res.send("API is running");
+});
+
+app.route("/api/products")
+.get(function(req, res){
+    res.json(products);
+});
+
+app.route("/api/products/:id")
+.get(function(req, res){
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+});
+
+
 app.listen(process.env.PORT || 5000, function() {
     console.log("Server started successffuly on port 5000");
   });
