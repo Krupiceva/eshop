51a19f97cb9c935e80ef78438937a88188eb529a
krupiceva <dora.mlinaric94@gmail.com>
2021-10-07 14:47:04 +0200

    First fronted simple version

---
+++ fronted/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+
+function Footer() {
+
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">
+                        Copyright &copy; eShop {year}
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
+
+export default Footer;
