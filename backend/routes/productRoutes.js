4099c4ab2e80dc15a3b92f28f06d20dcd3d17e32
krupiceva <dora.mlinaric94@gmail.com>
2021-10-13 14:41:50 +0200

    Implement product controller for routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,12 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
-
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
+import { getProducts, getProductById } from "../controllers/productController.js";
+ 
 router.route("/")
-.get(asyncHandler(async function(req, res){
-    const products = await Product.find({});
-    res.json(products);
-}));
+.get(getProducts);
 
 
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
 router.route("/:id")
-.get(asyncHandler(async function(req, res){
-    const product = await Product.findById(req.params.id);
-    
-    if(product){
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error("Product not found!");
-    }
-}));
+.get(getProductById);
 
 export default router;
