309e0ccc4ddf502860641e51b589c83969d196c0
krupiceva <dora.mlinaric94@gmail.com>
2021-12-03 18:32:00 +0100

    Order controller  and routes implement

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -45,6 +45,11 @@ const orderSchema = mongoose.Schema({
         required: true,
         default: 0.0
     },
+    withoutTaxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
     totalPrice: {
         type: Number,
         required: true,
