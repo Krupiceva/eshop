919669b9bd26000505831cae7fe4985cbbac5518
krupiceva <dora.mlinaric94@gmail.com>
2021-12-03 11:53:55 +0100

    Shipping Screen implement

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -10,6 +10,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
     return (
@@ -20,6 +21,7 @@ function App() {
                     <Route path="/register" component={RegisterScreen}  />
                     <Route path="/login" component={LoginScreen}  />
                     <Route path="/profile" component={ProfileScreen}  />
+                    <Route path="/shipping" component={ShippingScreen}  />
                     <Route path="/product/:id" component={ProductScreen}  />
                     <Route path="/cart/:id?" component={CartScreen} />
                     <Route path="/" component={HomeScreen} exact />
