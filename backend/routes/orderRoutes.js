b065abeb143729d78ab5a07d8ad7e9039737da87
krupiceva <dora.mlinaric94@gmail.com>
2021-12-06 20:00:06 +0100

    Paying order backend part

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderById } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById, updateOrderToPaid } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
  
 router.route("/")
@@ -9,7 +9,7 @@ router.route("/")
 router.route("/:id")
 .get(protect, getOrderById);
 
-
-
+router.route("/:id/pay")
+.put(protect, updateOrderToPaid);
 
 export default router;
