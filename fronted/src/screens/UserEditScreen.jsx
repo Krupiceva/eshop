7cebb0f206b10fb27b8b69a0003baad547372315
krupiceva <dora.mlinaric94@gmail.com>
2021-12-07 23:11:31 +0100

    Update user by admin implement

--- fronted/src/screens/UserEditScreen.jsx
+++ fronted/src/screens/UserEditScreen.jsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import FormContainer from '../components/FormContainer';
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUser } from "../actions/userActions";
+import { USER_UPDATE_RESET } from "../constants/userConstants";
 
 function UserEditScreen(props) {
 
@@ -20,20 +21,33 @@ function UserEditScreen(props) {
     const userDetails = useSelector(state => state.userDetails);
     const { loading, error, user } = userDetails;
 
+    const userUpdate = useSelector(state => state.userUpdate);
+    const { loading:loadingUpdate, error:errorUpdate, success:successUpdate } = userUpdate;
+
 
     useEffect(() => {
-        if(!user.name || user._id !== userId){
-            dispatch(getUserDetails(userId));
+        if(successUpdate){
+            dispatch({ type: USER_UPDATE_RESET });
+            history.push("/admin/userlist");
         } else {
-            setName(user.name);
-            setEmail(user.email);
-            setIsAdmin(user.isAdmin);
+            if(!user.name || user._id !== userId){
+                dispatch(getUserDetails(userId));
+            } else {
+                setName(user.name);
+                setEmail(user.email);
+                setIsAdmin(user.isAdmin);
+            }
         }
-    }, [user, dispatch, userId]);
+    }, [user, dispatch, history, userId, successUpdate]);
 
     function submitHandler(e){
         e.preventDefault();
-        
+        dispatch(updateUser({ 
+            _id: user._id,
+            name,
+            email,
+            isAdmin
+        }));
     }
 
     return (
@@ -41,6 +55,8 @@ function UserEditScreen(props) {
             <Link to={"/admin/userlist"} className="btn btn-light my-3">Go Back</Link>
             <FormContainer>
                 <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
                 {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
                     <Form onSubmit={submitHandler}>
                         <Form.Group controlId="name">
