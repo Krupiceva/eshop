ecc76e8f2c33eecacca8f8ddc7a0237ad2eec0f7
krupiceva <dora.mlinaric94@gmail.com>
2021-12-11 16:23:35 +0100

    Prepare for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,6 @@ if(process.env.NODE_ENV === "development"){
     app.use(morgan("dev"));
 }
 
-app.route("/")
-.get(function(req, res){
-    res.send("API is running...");
-});
 
 app.use(express.json());
 
@@ -35,6 +31,21 @@ app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/fronted/build")));
+
+    app.route("*")
+    .get(function(req, res){
+        res.sendFile(path.resolve(__dirname, "fronted", "build", "index.html"));
+    })
+} else {
+    app.route("/")
+    .get(function(req, res){
+        res.send("API is running...");
+    });
+}
+
 
 app.use(notFound);
 
